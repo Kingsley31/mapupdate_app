9daba7326a789453800b22414a7c0584335f8a24
Kingsley31 <kingsleyemeka31@gmail.com>
2021-04-09 12:58:06 +0100

    Removed the google map api key from the app

--- lib/constants/google_map_constants.dart
+++ lib/constants/google_map_constants.dart
@@ -7,7 +7,7 @@ class GoogleMapConstants{
   static const double CAMERA_TILT = 80;
   static const double CAMERA_BEARING = 30;
 
-  static const String GOOGLE_API_KEY = 'AIzaSyDUgNPYSKHrk6ZBv6T7wAFEocqlaQXm4rM';
+  static const String GOOGLE_API_KEY = 'ENTER_YOUR_GOOGLE_MAP_API_KEY_HERE';
 
   static const Color POLYLINE_COLOR = Color.fromARGB(255, 40, 122, 198);
 }
